b5cb2736c1cb74d91c79d94cefaf7871e3cfa255
Farhan Tahmid <farhantahmid881@gmail.com>
2022-06-16 23:03:50 +0600

    page connection through django

--- Ritu_Web_App/Ritu_Web_App/urls.py
+++ Ritu_Web_App/Ritu_Web_App/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from Ritu_Web_App.views import home, login
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
-    path('login/',views.login),
+    path('login/',views.login, name='login'),
 ]
