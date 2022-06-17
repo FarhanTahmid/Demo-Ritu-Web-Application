2bdbbd485a00d059a5b66dd2af77170e7972cc9f
Farhan Tahmid <farhantahmid881@gmail.com>
2022-06-17 11:10:35 +0600

    css integration in django

--- Ritu_Web_App/Ritu_Web_App/views.py
+++ Ritu_Web_App/Ritu_Web_App/views.py
@@ -4,4 +4,6 @@ def home(request):
     return render(request,'homepage.html')
 def login(request):
     return render(request,'loginPage.html')
+def signUp(request):
+    return render(request,'signUpForm.html')
 
