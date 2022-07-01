e160cd3966fc2b8ab383377cd210fcdf53144f56
Farhan Tahmid <farhantahmid881@gmail.com>
2022-07-01 09:41:09 +0600

    redo of project structure

--- Ritu_Web_App/Ritu_Web_App/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request,'homepage.html')
-def login(request):
-    return render(request,'loginPage.html')
-def signUp(request):
-    return render(request,'signUpForm.html')
-
