f13c66e2945a11c88afab9e82364d43ab676ddcf
Farhan Tahmid <farhantahmid881@gmail.com>
2022-07-01 10:29:39 +0600

    load of statics

--- Ritu_Web/Ritu_Web_App/urls.py
+++ Ritu_Web/Ritu_Web_App/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.homePage,name='homepage')
+    path('',views.homePage,name='homepage'),
+    path('login',views.loginPage,name='loginPage'),
+    path('signUp',views.signupPage,name='signUpPage'),
+    path('aboutUs',views.aboutUs,name='aboutUs'),
+    path('contactUs',views.contactUs,name='contactUs')
 ]
--- Ritu_Web/Ritu_Web_App/views.py
+++ Ritu_Web/Ritu_Web_App/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 def homePage(request):
     return render(request,'homepage.html')
+def loginPage(request):
+    return render(request,'loginPage.html')
+def signupPage(request):
+    return render(request,'signUpForm.html')
+def aboutUs(request):
+    return render(request,'aboutUs.html')
+def contactUs(request):
+    return render(request,'contactUs.html')
