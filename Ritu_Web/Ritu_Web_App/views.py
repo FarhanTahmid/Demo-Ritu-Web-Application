bb1110909e1315302f6e0a85693f1bfeb788c258
Farhan Tahmid <farhantahmid881@gmail.com>
2022-07-01 11:03:39 +0600

    loading all pages

--- Ritu_Web/Ritu_Web_App/views.py
+++ Ritu_Web/Ritu_Web_App/views.py
@@ -11,3 +11,21 @@ def aboutUs(request):
     return render(request,'aboutUs.html')
 def contactUs(request):
     return render(request,'contactUs.html')
+def finalMessage(request):
+    return render(request, 'FinalMessege.html')
+def leaderboard(request):
+    return render(request, 'Leaderboard.html')
+def marketplace(request):
+    return render(request, 'Marketplace.html')
+def menu(request):
+    return render(request, 'menu.html')
+def task1(request):
+    return render(request, 'task1.html')
+def task2(request):
+    return render(request, 'task2.html')
+def task3(request):
+    return render(request, 'task3.html')
+def verification(request):
+    return render(request,'verificationPage.html')
+def profileCard(request):
+    return render(request,'profileCard.html')
