7f972b9d0ddb062904c8822841486cd181aafa16
username <username@example.com>
2022-08-21 21:11:23 +0600

    Registered Task Text Tables Under Admin

--- ritu_web_app/admin.py
+++ ritu_web_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Task1Text, Task2Text, Task3Text
 
-# Register your models here.
+admin.site.register(Task1Text)
+admin.site.register(Task2Text)
+admin.site.register(Task3Text)
+
