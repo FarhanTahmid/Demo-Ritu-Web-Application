4109f7b6c602ece703fbb5530d9ea759f866e891
Farhan Tahmid <farhantahmid881@gmail.com>
2022-08-23 22:30:24 +0600

    html css js ready and timer set

--- ritu_web_app/static/task.js
+++ ritu_web_app/static/task.js
@@ -2,19 +2,11 @@ $(document).ready(function(){
 
     // typing text animation script
         var typed = new Typed(".typing", {
-            strings: ["Whoever completes first gets more points! You can access our marketplace to order a plant if you cannot go buy one yourself."],
-            typeSpeed: 150,
-            backSpeed: 100,
+            strings: [ "Whoever completes first gets more points!","You can access our marketplace to order a material if you cannot go buy one yourself"],
+            typeSpeed: 50,
+            backSpeed: 70,
             loop: true
         });
-
-    // typing text animation script
-    var typed = new Typed(".typing_1", {
-        strings: ["You can access our marketplace to order a plant if you cannot go buy one yourself."],
-        typeSpeed: 150,
-        backSpeed: 100,
-        loop: true
-    });
   
   });
 
