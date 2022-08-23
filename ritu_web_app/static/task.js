02c8b7a998bcd9273d3fa669aa7e5d060c59b0a2
username <username@example.com>
2022-08-23 08:03:35 +0600

    Fixed leaderboard, marketplace, final-message

---
+++ ritu_web_app/static/task.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    // typing text animation script
+        var typed = new Typed(".typing", {
+            strings: ["Whoever completes first gets more points! You can access our marketplace to order a plant if you cannot go buy one yourself."],
+            typeSpeed: 150,
+            backSpeed: 100,
+            loop: true
+        });
+  
+  });
