8bd27e7aba3b1d9d708661632f4ad7b6a04533f7
username <username@example.com>
2022-08-23 11:10:33 +0600

    Fixed tasks

--- ritu_web_app/static/task.js
+++ ritu_web_app/static/task.js
@@ -7,5 +7,15 @@ $(document).ready(function(){
             backSpeed: 100,
             loop: true
         });
+
+    // typing text animation script
+    var typed = new Typed(".typing_1", {
+        strings: ["You can access our marketplace to order a plant if you cannot go buy one yourself."],
+        typeSpeed: 150,
+        backSpeed: 100,
+        loop: true
+    });
   
   });
+
+
