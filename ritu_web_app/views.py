f1f4dc52f9fd2af84dfea38c179b3b7314c50a04
Farhan Tahmid <farhantahmid881@gmail.com>
2022-08-24 00:01:38 +0600

    points algorithm done, points table done

--- ritu_web_app/views.py
+++ ritu_web_app/views.py
@@ -1,10 +1,12 @@
+from tokenize import Double
 from xmlrpc.client import DateTime
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from . import addTextTask
 from .models import Marketplace, Players
 from django.core import serializers
-import datetime
+from datetime import datetime
+import time
 
 def homepage(request):
     """The home page for ritu"""
@@ -28,7 +30,19 @@ def task1(request):
     if request.method=="POST":
         text=request.POST['task_1_url']
         player = request.user.username
-        points='25.5'
+        
+        #Calculating score
+        timeInMilli = int(round(time.time() * 1000)) #getting current time
+        
+        date_obj = datetime.strptime('01.09.2022 00:00:00,00','%d.%m.%Y %H:%M:%S,%f') #getting the time of when the task was launched 
+        endTimeinMilli =int(date_obj.timestamp() * 1000)
+        
+        gapOfTimeInMilli=endTimeinMilli-timeInMilli #findin the time gap 
+        
+        point=round(0.00000014452*float(gapOfTimeInMilli),5) #rounding off points to 5 decimal digits
+        points=str(point)
+        
+        #populating table with datas
         addingUrl=addTextTask.TextUrl(points,text,player)
         addingUrl.addTextUrl_1()
         return redirect('ritu_web_app:task2')
