95983151e3d7674c30a1cbf1270c0dd4a27df2d5
Farhan Tahmid <farhantahmid881@gmail.com>
2022-08-26 12:49:30 +0600

    profile data done

--- ritu_web_app/player.py
+++ ritu_web_app/player.py
@@ -1,8 +1,31 @@
+from audioop import add
 from .models import Players
 class Player:
     def __init__(self,player):
         self.player=player
     def getInfo(self):
         playerData=Players.objects.raw("SELECT * FROM ritu_web_app_players WHERE username='"+self.player+"'")
-        print(playerData)
         
+        username=''
+        name=''
+        mobileNum=''
+        email=''
+        address=''
+        picture=''
+        
+        for data in playerData:
+            username=data.username
+            name=data.fullname
+            mobileNum=data.mobileNumber
+            email=data.email
+            address=data.address
+            picture=data.picture
+        data={
+            'username':username,
+            'name':name,
+            'mobileNum':mobileNum,
+            'email':email,
+            'address':address,
+            'picture':picture
+        }    
+        return data
