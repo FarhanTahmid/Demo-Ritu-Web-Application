3746ffe5e6cc00853bbbd3e73fd2e8e698f1b986
Farhan Tahmid <farhantahmid881@gmail.com>
2022-08-27 19:14:28 +0600

    orders database done and tested

---
+++ ritu_web_app/static/FinalMessage.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    // typing text animation script
+        var typed = new Typed(".typing", {
+            strings: [ "Please wait until the timer counts down to see the winner!"],
+            typeSpeed: 150,
+            backSpeed: 100,
+            loop: true
+        });
+  
+  });
---
+++ ritu_web_app/static/menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    // typing text animation script
+        var typed = new Typed(".typing", {
+            strings: [ "Heal The World", "Make It A Better Place"],
+            typeSpeed: 150,
+            backSpeed: 100,
+            loop: true
+        });
+  
+  });
